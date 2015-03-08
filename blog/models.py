8e09605fd6189c4fc458aaedb22c8cee207ff095
chenxc86 <chenxiaochen86@hotmail.com>
2015-03-08 15:20:45 +0800

    去除了update_time字段的auto_now参数
    去除update_time字段的auto_now参数，用以修正文章阅读量更新与文章内容更新时间冲突的问题

--- blog/models.py
+++ blog/models.py
@@ -62,7 +62,7 @@ class Article(models.Model):
     # 文章发布时间
     publish_time = models.DateTimeField(auto_now_add=True, verbose_name=u"文章发布时间")
     # 文章最后更新时间
-    update_time = models.DateTimeField(auto_now=True, verbose_name=u"文章更新时间")
+    update_time = models.DateTimeField(verbose_name=u"文章更新时间")
     # 文章内容
     content = models.TextField(verbose_name=u"文章内容")
     # 文章状态
